50870a172dc11f9110e1c0b194057215b29079bd
ming0604 <b0981340647@gmail.com>
2023-12-12 06:17:24 +0800

    fail use map2D

--- src/scan.cpp
+++ src/scan.cpp
@@ -7,6 +7,7 @@
 #include <geometry_msgs/PoseWithCovarianceStamped.h>
 
 #include <tf/tf.h>
+#include <tf/transform_listener.h>
 #include <sensor_msgs/PointCloud2.h>
 
 #include <pcl_ros/point_cloud.h>
@@ -27,22 +28,34 @@ private:
     float amcl_pose_y;
     float amcl_pose_yaw;
     nav_msgs::OccupancyGrid grid_map;
+    vector<vector<int>> map_2D;
+    float grid_origin_x;
+    float grid_origin_y;
+    int grid_height;
+    int grid_width;
     float laser_angle_min;
     float laser_angle_max;
     float laser_angle_increment;
+    double laser_x;
+    double laser_y;
+    int laser_x_grid;
+    int laser_y_grid;
+    double laser_yaw;
+    float occ_threshold=65;
     vector<float> ranges;
     vector<float> intensities;
 
     ros::NodeHandle _nh;
 
-    // 订阅激光扫描数据
+    // subscibe laser scan
     ros::Subscriber laser_scan_sub;
     //subscibe amcl pose
     ros::Subscriber poseSub; 
 
     ros::Publisher scan_pc_pub;
-
-
+    ros::Publisher virtual_pc_pub;
+    tf::TransformListener listener;
+    tf::StampedTransform laser_map_transform;
 public:
     scan_to_pc(ros::NodeHandle nh)
     {   
@@ -52,12 +65,234 @@ public:
         poseSub = _nh.subscribe("/amcl_pose", 1, &scan_to_pc::amclposeCallback,this);
 
         scan_pc_pub = _nh.advertise<sensor_msgs::PointCloud2>("/scan_pc", 100);
+        virtual_pc_pub = _nh.advertise<sensor_msgs::PointCloud2>("/virtual_pc", 100);
 
     }
 
-    pcl::PointCloud<pcl::PointXYZ>::Ptr create_vitual_scan_pc()
+    //change map from 1D arry into 2D vector
+    vector<vector<int>> convertTo2DVector(vector<signed char> data, int rows, int cols) {
+        vector<vector<int>> result;
+        int index = 0;
+
+        for (int i = 0; i < rows; i++) 
+        {
+            vector<int> row_data(data.begin() + index, data.begin()+index+cols-1);
+            result.push_back(row_data);
+            index += cols;
+        }
+        return result;
+    }
+
+    void tf_listener()
     {
+        try{
+            //"map is target,laser is source"
+            listener.lookupTransform("map", "laser", ros::Time(0),laser_map_transform);
+        }
+        catch (tf::TransformException& ex) {
+            ROS_ERROR("%s", ex.what());
+            // 处理异常
+            return;
+        }
         
+        laser_x = laser_map_transform.getOrigin().x(),
+        laser_y = laser_map_transform.getOrigin().y(),
+        laser_yaw = tf::getYaw(laser_map_transform.getRotation());
+        ROS_INFO("get lidar pose:  x: %f, y: %f, yaw: %f",laser_x,laser_y,laser_yaw);
+    }
+
+    float line_fx(int x,float m)
+    {
+        float y;
+        y = m*(x-laser_x_grid)+laser_y_grid;
+
+        return y;
+    }
+
+    float line_fy(int y,float m)
+    {
+        float x;
+        x = (1/m)*(y-laser_y_grid)+laser_x_grid;
+
+        return x;
+    }
+
+    pcl::PointCloud<pcl::PointXYZ>::Ptr create_vitual_scan_pc()
+    {   
+        pcl::PointCloud<pcl::PointXYZ>::Ptr pc(new pcl::PointCloud<pcl::PointXYZ>);
+        pcl::PointXYZ point;
+        tf_listener();
+        laser_x_grid = (laser_x-grid_origin_x)/grid_map.info.resolution;
+        laser_y_grid = (laser_y-grid_origin_y)/grid_map.info.resolution;
+        cout << "laser_x_grid = " << laser_x_grid << ",laser_y_grid = " << laser_y_grid << ", laser_yaw="<< laser_yaw << endl;
+        float distance_virtual;
+       
+        
+        for(int i=0; i<ranges.size(); i++)
+        {   
+            int x_grid = laser_x_grid;
+            int y_grid = laser_y_grid;
+            float angle,x_map,y_map,m;
+            bool find_vir_flag=false;
+            angle = laser_yaw + laser_angle_min + i * laser_angle_increment;
+            cout << "angle:" << angle << endl;
+            if(angle>2.0*M_PI)
+            {
+                angle = angle - 2.0*M_PI;
+            }
+            m = tan(angle);
+            cout<<m<<endl;
+            //for 1＆2 Quadrant
+            if(angle>=0 && angle<=M_PI)
+            {   
+                
+                if(m>=0 && m<=1)
+                {   
+                    while(x_grid>=0 && x_grid < grid_width && y_grid>=0 && y_grid < grid_height) 
+                    {
+                        x_grid = x_grid+1;
+                        y_grid = round(line_fx(x_grid, m));
+                        if(map_2D[y_grid][x_grid]>=occ_threshold)
+                        {   
+                            //virtual point at map frame
+                            x_map = x_grid * grid_map.info.resolution + grid_origin_x;
+                            y_map = y_grid * grid_map.info.resolution + grid_origin_y;
+                            find_vir_flag = true;
+                            break;
+                        }
+                    }
+                }
+                else if(m>1)
+                {
+                    while(x_grid>=0 && x_grid < grid_width && y_grid>=0 && y_grid < grid_height) 
+                    {
+                        y_grid = y_grid+1;
+                        x_grid = round(line_fy(y_grid, m));
+                        if(map_2D[y_grid][x_grid]>=occ_threshold)
+                        {   
+                            //virtual point at map frame
+                            x_map = x_grid * grid_map.info.resolution + grid_origin_x;
+                            y_map = y_grid * grid_map.info.resolution + grid_origin_y;
+                            find_vir_flag = true;
+                            break;
+                        }
+                    }
+                }
+                else if(m<-1)
+                {
+                    while(x_grid>=0 && x_grid < grid_width && y_grid>=0 && y_grid < grid_height) 
+                    {
+                        y_grid = y_grid+1;
+                        x_grid = round(line_fy(y_grid, m));
+                        if(map_2D[y_grid][x_grid]>=occ_threshold)
+                        {   
+                            //virtual point at map frame
+                            x_map = x_grid * grid_map.info.resolution + grid_origin_x;
+                            y_map = y_grid * grid_map.info.resolution + grid_origin_y;
+                            find_vir_flag = true;
+                            break;
+                        }
+                    }
+                }
+                else if(m<0 && m>=-1)
+                {
+                    while(x_grid>=0 && x_grid < grid_width && y_grid>=0 && y_grid < grid_height) 
+                    {
+                        x_grid = x_grid-1;
+                        y_grid = round(line_fx(x_grid, m));
+                        if(map_2D[y_grid][x_grid]>=occ_threshold)
+                        {   
+                            //virtual point at map frame
+                            x_map = x_grid * grid_map.info.resolution + grid_origin_x;
+                            y_map = y_grid * grid_map.info.resolution + grid_origin_y;
+                            find_vir_flag = true;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            else
+            {
+                if(m>=0 && m<=1)
+                {
+                    while(x_grid>=0 && x_grid < grid_width && y_grid>=0 && y_grid < grid_height) 
+                    {
+                        x_grid = x_grid-1;
+                        y_grid = round(line_fx(x_grid, m));
+                        if(map_2D[y_grid][x_grid]>=occ_threshold)
+                        {   
+                            //virtual point at map frame
+                            x_map = x_grid * grid_map.info.resolution + grid_origin_x;
+                            y_map = y_grid * grid_map.info.resolution + grid_origin_y;
+                            find_vir_flag = true;
+                            break;
+                        }
+                    }
+                }
+                else if(m>1)
+                {
+                    while(x_grid>=0 && x_grid < grid_width && y_grid>=0 && y_grid < grid_height) 
+                    {
+                        y_grid = y_grid-1;
+                        x_grid = round(line_fy(y_grid, m));
+                        if(map_2D[y_grid][x_grid]>=occ_threshold)
+                        {   
+                            //virtual point at map frame
+                            x_map = x_grid * grid_map.info.resolution + grid_origin_x;
+                            y_map = y_grid * grid_map.info.resolution + grid_origin_y;
+                            find_vir_flag = true;
+                            break;
+                        }
+                    }
+                }
+                else if(m<-1)
+                {   ROS_INFO("start find virtual point ");
+                    while(x_grid>=0 && x_grid <= grid_width && y_grid>=0 && y_grid <= grid_height) 
+                    {   
+                        
+                        y_grid = y_grid-1;
+                        x_grid = round(line_fy(y_grid, m));
+                        if(map_2D[y_grid][x_grid]>=occ_threshold)
+                        {   
+                            //virtual point at map frame
+                            x_map = x_grid * grid_map.info.resolution + grid_origin_x;
+                            y_map = y_grid * grid_map.info.resolution + grid_origin_y;
+                            find_vir_flag = true;
+                            break;
+                        }
+                    }
+                }
+                else if(m<0 && m>=-1)
+                {
+                    while(x_grid>=0 && x_grid <= grid_width && y_grid>=0 && y_grid <= grid_height) 
+                    {
+                        x_grid = x_grid+1;
+                        y_grid = round(line_fx(x_grid, m));
+                        if(map_2D[y_grid][x_grid]>=occ_threshold)
+                        {   
+                            //virtual point at map frame
+                            x_map = x_grid * grid_map.info.resolution + grid_origin_x;
+                            y_map = y_grid * grid_map.info.resolution + grid_origin_y;
+                            find_vir_flag = true;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            //change into point cloud in map frame
+            if(find_vir_flag)
+            {
+                point.x = x_map;
+                point.y = y_map;
+                point.z = 0.0;  
+                pc->push_back(point);
+            }
+        }
+        
+        ROS_INFO("virtual pc has been created");
+        return pc;
     }
 
     pcl::PointCloud<pcl::PointXYZ>::Ptr create_scan_pc()
@@ -82,7 +317,7 @@ public:
             point.y = y;
             point.z = 0.0;  
             pc->push_back(point);
-            ROS_INFO("scan to pc %d complete",i);
+            //ROS_INFO("scan to pc %d complete",i);
         }
 
         ROS_INFO("scan to pc complete");
@@ -120,25 +355,40 @@ public:
         {
             // 在这里处理获取到的栅格地图
             grid_map = mapsrv.response.map;
-            // 可以在这里执行你的逻辑操作
+            grid_origin_x = grid_map.info.origin.position.x;
+            grid_origin_y = grid_map.info.origin.position.y;
+            grid_height = grid_map.info.height;
+            grid_width = grid_map.info.width;
             ROS_INFO("Received Map: width=%d, height=%d, resolution=%.3f\n", grid_map.info.width, grid_map.info.height, grid_map.info.resolution);
+            ROS_INFO("Received Map: origin_x=%.3f, origin_y=%.3f\n", grid_origin_x, grid_origin_y);
         }
         else
         {
             ROS_ERROR("Failed to call static_map service");
         }
-        
+
+        map_2D = convertTo2DVector(grid_map.data, grid_map.info.height, grid_map.info.width);
+        cout << "map to 2D vector complete, size=" <<map_2D.size()<< endl;
+
         pcl::PointCloud<pcl::PointXYZ>::Ptr scan_pc = create_scan_pc();
         ROS_INFO("Received scan_pc");
-        sensor_msgs::PointCloud2 pc_msg;
-        pcl::toROSMsg(*scan_pc, pc_msg);
-        pc_msg.header.stamp = ros::Time::now();
-        pc_msg.header.frame_id = "laser";
-        scan_pc_pub.publish(pc_msg);
+        sensor_msgs::PointCloud2 scan_pc_msg;
+        pcl::toROSMsg(*scan_pc, scan_pc_msg);
+        scan_pc_msg.header.stamp = ros::Time::now();
+        scan_pc_msg.header.frame_id = "laser";
+        scan_pc_pub.publish(scan_pc_msg);
         ROS_INFO("scan_pc published");
 
-
-
+        pcl::PointCloud<pcl::PointXYZ>::Ptr virtual_pc = create_vitual_scan_pc();
+        /*
+        ROS_INFO("Received virtual_pc");
+        sensor_msgs::PointCloud2 virtual_pc_msg;
+        pcl::toROSMsg(*virtual_pc, virtual_pc_msg);
+        virtual_pc_msg.header.stamp = ros::Time::now();
+        virtual_pc_msg.header.frame_id = "map";
+        virtual_pc_pub.publish(virtual_pc_msg);
+        ROS_INFO("virtual_pc published");
+        */
     }
 };
 
